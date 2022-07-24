459907a300c11f05b92cc4d79b57ca40c8fd2c43
yurenjiedeta <1293252163@qq.com>
2022-07-24 22:32:40 +0800

    还原index.js

--- rt-17.0.0/src/index.js
+++ rt-17.0.0/src/index.js
@@ -1,37 +1,13 @@
-const React = window.React;
+import './polyfills';
+import loadReact from './react-loader';
 
-class VersionPicker extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    const version = props.version || 'local';
-    const versions = [version];
-    this.state = {versions};
-  }
+loadReact()
+  .then(() => import('./components/App'))
+  .then(App => {
+    const {React, ReactDOM} = window;
 
-  componentWillMount() {
-    getVersionTags().then(tags => {
-      let versions = tags.map(tag => tag.name.slice(1));
-      versions = [`local`, ...versions];
-      this.setState({versions});
-    });
-  }
-
-  onChange = event => {
-    this.props.onChange(event.target.value);
-  };
-
-  render() {
-    const {version, id, name} = this.props;
-    const {versions} = this.state;
-
-    return (
-      <select id={id} name={name} value={version} onChange={this.onChange}>
-        {versions.map(version => (
-          <option key={version} value={version}>
-            {version}
-          </option>
-        ))}
-      </select>
+    ReactDOM.render(
+      React.createElement(App.default),
+      document.getElementById('root')
     );
-  }
-}
+  });
