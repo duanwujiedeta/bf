31666f431aea0ae51b39804a3231ea1a46c270bf
yurenjiedeta <1293252163@qq.com>
2024-02-23 17:37:58 +0800

    webjson

--- web.js
+++ web.js
@@ -1,4 +1,13 @@
-plugins: [
-    new WebpackManifestPlugin(),
-    // ...
-]
+if (document.getElementById('ckeditor')) {
+    import('@ckeditor/ckeditor5-
+build - classic').then(function(ClassicEditor){
+ClassicEditor.default
+            .create(document.querySelector('#ckeditor'))
+            .then(editor => {
+                console.log(editor);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+});
+}
