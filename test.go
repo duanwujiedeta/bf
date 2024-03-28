7390b75dfd244dbb971d8f029d418cae932b5add
yurenjiedeta <1293252163@qq.com>
2024-03-28 18:40:11 +0800

    go1

--- test.go
+++ test.go
@@ -1,51 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
 )
 
-// "math/cmplx"
-
-type person struct {
-	name string
-	age  int
-	pet  string
-}
+type MyInt int
 
 func main() {
-	result, remainder, err := divAndRemainder(5, 2)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println(result, remainder)
-}
-func addTo(base int, vals ...int) []int {
-	out := make([]int, 0, len(vals))
-	for _, v := range vals {
-		out = append(out, base+v)
-	}
-	return out
-}
-
-func div(numerator int, denominator int) int {
-	if denominator == 0 {
-		return 0
-	}
-	return numerator / denominator
-}
-
-type MyFuncOpts struct {
-	FirstName string
-	LastName  string
-	Age       int
-}
-
-func divAndRemainder(numerator int, denominator int) (int, int, error) { // 多返回值的定义
-	if denominator == 0 {
-		return 0, 0, errors.New("cannot divide by zero")
-	}
-	return numerator / denominator, numerator % denominator, nil // 多返回值的 return
+	var i interface{}
+	var mine MyInt = 20
+	i = mine
+	i2 := i.(int)
+	fmt.Println(i2)
 }
