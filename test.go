c1eaa7ea925e4499f6bed632c83799b7c09ce72e
yurenjiedeta <1293252163@qq.com>
2024-03-29 18:22:46 +0800

    error interface

--- test.go
+++ test.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 type MyInt int
 
 func main() {
-	var i interface{}
-	var mine MyInt = 20
-	i = mine
-	i2 := i.(int)
-	fmt.Println(i2)
+	numerator := 20
+	denominator := 0
+	remainder, mod, err := calcRemainderAndMod(numerator, denominator)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(remainder, mod)
+}
+func calcRemainderAndMod(numerator, denominator int) (int, int, error) {
+	if denominator == 0 {
+		return 0, 0, errors.New("denominator is 0") //error 的返回
+	}
+	return numerator / denominator, numerator % denominator, nil
+}
+
+type error interface { // error 的 interface 的定义
+	Error() string
 }
