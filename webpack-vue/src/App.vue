a875b44f9f9b2609d08be1c49b4bdcbd479ae9d9
luyigen <luyigen@empowerwin.com>
2024-07-10 16:07:15 +0800

    wv

---
+++ webpack-vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+        <h1>Hello, Vue 2.6 with Webpack!</h1>
+        <button @click="loadComponent">Load Component</button>
+        <component :is="componentToShow"></component>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            componentToShow: null,
+        };
+    },
+    methods: {
+        async loadComponent() {
+            if (!this.componentToShow) {
+                const { default: LazyComponent } = await import('./LazyComponent.vue');
+                this.componentToShow = LazyComponent;
+            }
+        },
+    },
+};
+</script>
+
+<style scoped lang="less">
+#app {
+    text-align: center;
+    color: #42b983;
+
+    button {
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+}
+</style>
---
+++ webpack-vue/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
+
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        devtool: isDevelopment ? 'source-map' : 'none',
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'less-loader'
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
+            },
+            extensions: ['*', '.js', '.vue', '.json']
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.html',
+                inject: true,
+            }),
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000,
+        },
+    };
+};
